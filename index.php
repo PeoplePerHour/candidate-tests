2d617c73fcc85a95173f6e5707f69c20345792a5
John Christofis <ioannis.christofis@gmail.com>
2018-03-11 22:47:42 +0000

    Perform select, update and transactions on index.php

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php
 
-
-use johnchristofis\dbcmanager\MysqlDriver;
+use johnchristofis\dbcmanager\ConnectionManager;
 
 require_once 'config.php';
 
-$db = new MysqlDriver('localhost','root','root','dummydb',3306);
 
-//$select = $db->select('user',['username','password'],['lastname'=>'doe'],['orderBy'=>'id','order'=>'asc','limit'=>1,'offset'=>1]);
+$db = ConnectionManager::connect('mysql','localhost','root','root','dummydb',3306);
 
-//$insert = $db->insert('user',['firstname'=>'nikos','lastnames'=>'korompos','username'=>'nkorompos','password'=>'pass888']);
+/*
 
+try {
+    $db->beginTransaction();
+
+    // Insert records
+    $db->insert('user',['firstname'=>'nikos','lastname'=>'korompos','username'=>'nkorompos','password'=>'pass7788']);
+    $db->insert('user',['firstname'=>'nikos','lastname'=>'koukos','username'=>'nkoukos','password'=>'pass8899']);
+
+    // Assuming the both inserts work, we get to the following line.
+    $db->commitTransaction();
+
+} catch(PDOException $e) {
+    $db->rollBackTransaction();
+}
+
+
+$db->close();
+
+*/
