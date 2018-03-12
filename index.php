94631765717f5672aaa69d0a2ec5c5a4d7e0557e
John Christofis <ioannis.christofis@gmail.com>
2018-03-12 23:50:12 +0000

    Commented all Connection Manager operations

--- index.php
+++ index.php
@@ -1,29 +1,37 @@
 <?php
 
+/**
+ * File to perform testing on the Connection Manager class
+ */
+
 use johnchristofis\dbcmanager\ConnectionManager;
 
 require_once 'config.php';
 
 
-$db = ConnectionManager::connect('mysql','localhost','root','root','dummydb',3306);
+#$db = ConnectionManager::connect('mysql','localhost','root','root','dummydb',3306);
 
-/*
+#$db->delete('user',[]);
 
-try {
+
+/*try {
     $db->beginTransaction();
 
     // Insert records
     $db->insert('user',['firstname'=>'nikos','lastname'=>'korompos','username'=>'nkorompos','password'=>'pass7788']);
     $db->insert('user',['firstname'=>'nikos','lastname'=>'koukos','username'=>'nkoukos','password'=>'pass8899']);
+    $db->insert('user',['firstname'=>'john','lastname'=>'doe','username'=>'nkoukos','password'=>'pass123']);
+    $db->insert('user',['firstname'=>'jane','lastname'=>'doe','username'=>'janedoe','password'=>'pass345']);
+    $db->delete('user',['usernames'=>'johndoe']);
+    $db->delete('user',['username'=>'janedoe']);
 
     // Assuming the both inserts work, we get to the following line.
     $db->commitTransaction();
 
 } catch(PDOException $e) {
     $db->rollBackTransaction();
-}
+}*/
 
 
-$db->close();
+#$db->close();
 
-*/
