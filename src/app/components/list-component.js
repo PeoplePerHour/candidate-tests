c7f345cb72e11298ae9373fe2bdeb64721fbd594
George Roubie <g.roubie@sieben.gr>
2020-03-01 13:37:44 +0200

    create basic list

--- src/app/components/list-component.js
+++ src/app/components/list-component.js
@@ -3,12 +3,30 @@ import * as loaderComponent from './loader-component';
 // Application Services
 import * as apiHttpService from '../services/api-http-service';
 
+function createCharacterListTiles(charactersList) {
+  const listEl = document.getElementsByTagName('list')[0];
+  for (let i = 0; i < charactersList.length; i++) {
+    const listTileEl = document.createElement('div');
+    listTileEl.id = charactersList[i].id;
+    listTileEl.className = 'list-tile';
+    listTileEl.innerHTML = `
+      <img src="${charactersList[i].image}">
+      <div class="character-info">
+        <h3 class="character-name">${charactersList[i].name}</h3>
+      </div>
+    `;
+    listEl.appendChild(listTileEl);
+  }
+}
+
+
 function loadCharactersList(page) {
   // Show a loader
   loaderComponent.init();
   // Get characters list
-  apiHttpService.getCharactersList(page).then((charactersList) => {
-    console.log(charactersList);
+  apiHttpService.getCharactersList(page).then((charactersListInfo) => {
+    console.log(charactersListInfo);
+    createCharacterListTiles(charactersListInfo.results);
     // Remove loader
     loaderComponent.destroy();
   });
